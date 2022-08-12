533714c53da4b1436d09137be7dd47ac0ecfa324
AdigunaSuryo7 <suryo@nubela.co>
2022-08-12 20:29:05 +0700

    initial commit with jest

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/global.css'
+import '../styles/global.css'
 import type { AppProps } from 'next/app'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import UtilsProvider from '../context/utils';
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+  <UtilsProvider>
+    <Component {...pageProps} />
+  </UtilsProvider>
+  )
 }
+
+export default MyApp
